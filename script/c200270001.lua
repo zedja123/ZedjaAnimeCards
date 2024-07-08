c52b6e77b3b790d92522ae33e8cb62e7844bfa53
zedja123 <miguel.zedja@hotmail.com>
2024-07-08 12:43:56 -0300

    Immortal Class

---
+++ script/c200270001.lua
@@ -0,0 +1,40 @@
+--Immortal Class
+function c200270001.initial_effect(c)
+	-- Tribute 2 monsters to Special Summon 1 Level 7 or higher monster from your Graveyard, ignoring Summoning conditions
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+
+function s.costfilter(c)
+	return c:IsReleasable() and c:IsType(TYPE_MONSTER)
+end
+
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,s.costfilter,2,nil) end
+	local g=Duel.SelectReleaseGroup(tp,s.costfilter,2,2,nil)
+	Duel.Release(g,REASON_COST)
+end
+
+function s.spfilter(c,e,tp)
+	return c:IsLevelAbove(7) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,true,true,POS_FACEUP)
+	end
+end
